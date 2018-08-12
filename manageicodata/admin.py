50512ecb61e191069d585325d976fa7d031cf8fe
phil <javalake@xtra.co.nz>
2018-08-12 14:55:13 +1200

    Added files and changes required for deployment to heroku 2

--- manageicodata/admin.py
+++ manageicodata/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Blockchain, CapUnit, Currency, ICO, ICOInstance
 
+from django.http import HttpResponse
+from django.contrib import messages
+
 #admin.site.register(Blockchain)
 admin.site.register(CapUnit)
 admin.site.register(Currency)
@@ -23,12 +26,15 @@ class ICOAdmin(admin.ModelAdmin):
     list_display = ['symbol', 'name', 'display_blockchain']
     pass
 
+#
+      
+        
 # Register the Admin classes for ICOInstance using the decorator
 @admin.register(ICOInstance) 
 class ICOInstanceAdmin(admin.ModelAdmin):
+    actions = ['export_csv']
     list_filter = ['country', 'cap_unit', 'number_of_rounds',
                    'start_sale_date', 'last_sale_date', 'price_currency'] 
-    
     fieldsets = (
         (None, {
             'fields': ('id', 'ico', 'country')
@@ -50,6 +56,35 @@ class ICOInstanceAdmin(admin.ModelAdmin):
             }),
     ) 
     
-    pass
-    
-    
+    def export_csv(self, request, queryset):
+        import csv
+        from django.utils.encoding import smart_str
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=ico_instances.csv'
+        writer = csv.writer(response, csv.excel)
+        response.write(u'\ufeff'.encode('utf8')) 
+        # write column headings
+        writer.writerow([
+                smart_str(u"id"),
+                smart_str(u"ico"),
+                smart_str(u"country"),
+        ])
+        # write row contents
+        num_rows = len(queryset)
+        for obj in queryset:
+            writer.writerow([
+                smart_str(obj.pk),
+                smart_str(obj.ico),
+                smart_str(obj.country),
+            ])
+        msg = "{0} ICO Instances written to CSV.".format(num_rows)
+        messages.add_message(request, messages.INFO, msg)    
+        #self.message_user(request, "{0} ICO Instances written to CSV.".format(num_rows))
+        return response    
+    export_csv.short_description = "Export ICO Instances to CSV."
+
+
+
+
+
+
